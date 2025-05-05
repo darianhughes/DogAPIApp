ba26224f18c198c21e92eeb6f183ec60f7ebf20f
darianhughes <darianmhughes7@gmail.com>
2025-05-05 08:27:36 -0500

    Initial Commit

---
+++ DogAPIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DogAPIApp
+//
+//  Created by HUGHES, DARIAN M. on 5/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DogAPIApp/DogAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAPIAppApp.swift
+//  DogAPIApp
+//
+//  Created by HUGHES, DARIAN M. on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DogAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
